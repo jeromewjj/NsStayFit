52abf256ed6a95758bf20aab000edb72c6036888
yl-ang <yongliangang@gmail.com>
2022-06-11 14:43:55 +0800

    Add tailwind config

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
+import {TailwindProvider} from 'tailwind-rn';
+import utilities from './tailwind.json';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 import { AppNavigator } from './frontend/AppNavigator';
 
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <AppNavigator />
-  </ApplicationProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+        <TailwindProvider utilities={utilities}>
+          <AppNavigator />
+        </TailwindProvider>
+    </ApplicationProvider>
 );
