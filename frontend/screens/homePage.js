adbf7a7f4acc37c6f681459ff77fb2fdf8bdf409
yl-ang <yongliangang@gmail.com>
2022-06-11 15:56:40 +0800

    Add layouts to Main Page

--- frontend/screens/homePage.js
+++ frontend/screens/homePage.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {useTailwind} from 'tailwind-rn';
 import { Layout, Text } from '@ui-kitten/components';
 
 export default HomePage = () => {
-    return (
-        <Text category='h1'>Home Page!</Text>
-    );
+
+    const tailwind = useTailwind();
+
+    return(
+        <Layout style={tailwind('flex-col')}>
+            <Layout level='1'>
+                <Text>4</Text>
+            </Layout>
+            <Layout level='3'>
+                <Text>3</Text>
+            </Layout>
+            <Layout level='2'>
+                <Text>2</Text>
+            </Layout>
+            <Layout level='4'>
+                <Text>1</Text>
+            </Layout>
+        </Layout>
+    )
   }
