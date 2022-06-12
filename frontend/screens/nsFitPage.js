e23ae14386addd3d7695d1833726b5cf45dda5ef
Mikealeee <michael.yewchuan@gmail.com>
2022-06-12 18:32:32 +0800

    update

--- frontend/screens/nsFitPage.js
+++ frontend/screens/nsFitPage.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {useTailwind} from 'tailwind-rn';
-import { Layout, Text, Button, ViewPager, Popover, Modal, Card, Input} from '@ui-kitten/components';
-import { StyleSheet, View,  KeyboardAvoidingView, ScrollView } from 'react-native';
-
+import { Layout, Text, Button, ViewPager, Modal, Card, Input} from '@ui-kitten/components';
+import { Animated, View, ScrollView } from 'react-native';
 import { Video } from 'expo-av';
 
 export default NsFitPage = () => {
@@ -18,7 +17,46 @@ export default NsFitPage = () => {
     const [pushupsValue, setpushupsValue] = React.useState('');  // value of input from popup
     const [squatsValue, setsquatsValue] = React.useState('');  // value of input from popup
 
+    const [startExcerciseValue, setstartExcerciseValue] = React.useState('');  // value of input from popup
+    const [endExcerciseValue, setendExcerciseValue] = React.useState('');  // value of input from popup
+    
+    // get current day-month-date-time-year
+    const getDate = () => {
+      const currentDate = new Date;
+      const localDate = currentDate.toLocaleString('en-US', 'Asia/Singapore')
+      console.log(localDate);
+      return localDate;
+    };
 
+    // reset all data
+    const resetExcercisePlan = () => {
+      setburpeesValue('');
+      setweightLiftingValue('');
+      setpushupsValue('');
+      setsquatsValue('');
+      setstartExcerciseValue('');
+      setendExcerciseValue('');
+    };
+
+    // get data to backend
+    const sendData = () => {
+      const burpeesValue = burpeesValue;
+      const weightLiftingValue = weightLiftingValue;
+      const pushupsValue = pushupsValue;
+      const squatsValue = squatsValue;
+      const startExcerciseValue = startExcerciseValue;
+      const endExcerciseValue =  endExcerciseValue;
+
+      // pass in as props? and send to database whatever is releavant
+    };
+
+    const valEndExercise = () => {
+      if (startExcerciseValue === '') {
+        return 
+      }
+      setendExcerciseValue(getDate());
+
+    };
 
     return (
     <Layout style= {tailwind('flex-col flex-grow')} >    
@@ -28,12 +66,35 @@ export default NsFitPage = () => {
       </Layout>
       
       <Layout style={tailwind('items-center justify-center mt-5')}> 
-        <Text style={tailwind('text-xl')}> Swipe to select exercise and follow along</Text>  
-        <Text style={tailwind('text-2xl mt-2')}> Tap video to play it </Text>  
+        <Text style={tailwind('text-xl')}> Swipe for exercise videos and follow along</Text>  
+        <Text style={tailwind('text-2xl mt-2')}> Tap video to play </Text>  
       </Layout>
 
       <View style= {tailwind('flex-col flex-1 mt-8')}> 
         <ViewPager selectedIndex={selectedIndex} onSelect={index => setSelectedIndex(index)} style={{ flex:1,flexGrow:1}}>
+          <Layout style={tailwind('items-center justify-center mb-3')}>
+              <Text style={tailwind('font-bold text-3xl')}> Workout Session Plan </Text>
+
+              <Text style={{ lineHeight: 30 }}>{ }</Text>
+
+              <Card>
+                <Text> Burpees Reps  :  {burpeesValue} </Text>
+                <Text> Weights Reps  :  {burpeesValue} </Text>
+                <Text> Pushups Reps :  {pushupsValue} </Text>
+                <Text> Squats Reps    :  {squatsValue} </Text>
+                <Text> Start Time        :  {startExcerciseValue} </Text>
+                <Text> End Time          :  {endExcerciseValue} </Text>
+              </Card>
+
+              <Text style={{ lineHeight: 30 }}>{ }</Text>
+
+              <Button 
+                onPress={() => {setVisible(false); resetExcercisePlan()}} style={tailwind('mt-4 mx-4 rounded-lg p-2')}>
+                Reset Plan
+              </Button>
+
+          </Layout>
+        
           <Layout style={tailwind('items-center justify-center')} >
 
             <Text style={tailwind('font-bold text-3xl mb-3')}> Burpees </Text>
@@ -98,22 +159,12 @@ export default NsFitPage = () => {
             />  
           </Layout>
 
-          <Layout style={tailwind('items-center justify-center mb-3')}>
-              <Text style={tailwind('font-bold text-3xl')}> Workout Session  Plan </Text>
-              <Card>
-                <Text> Burpees Reps  :  {burpeesValue} </Text>
-                <Text> Weights Reps  :  {weightLiftingvalue} </Text>
-                <Text> Pushups Reps :  {pushupsValue} </Text>
-                <Text> Squats Reps    :  {squatsValue} </Text>
-              </Card>
-          </Layout>
-
         </ViewPager>
       </View>          
       
-      <View style={tailwind(' flex-grow items-center justify-center flex-row mt-8')}>
-        <Button style={tailwind('text-3xl mx-4')} onPress={() => setVisible(true)} > Start Workout </Button>  
-        <Button style={tailwind('text-3xl mx-4')}> End Workout </Button>    
+      <View style={tailwind(' flex-grow items-center justify-center flex-row mt-8 ')}>
+        <Button style={tailwind('text-3xl mx-4 rounded-lg p-4 ')} onPress={() => setVisible(true)} > Plan Workout </Button>  
+        <Button style={tailwind('text-3xl mx-4 rounded-lg p-4')} onPress={() => valEndExercise()} > End Workout </Button>    
       </View>
 
 
@@ -136,6 +187,7 @@ export default NsFitPage = () => {
             label='Burpees'
             placeholder='Enter desired number of reps'
             onChangeText={nextValue =>setburpeesValue(nextValue)}
+            maxLength={2}
             />
             
             <Text style={{ lineHeight: 30 }}>{ }</Text>
@@ -146,6 +198,7 @@ export default NsFitPage = () => {
             label='Weight Lifting'
             placeholder='Enter desired number of reps'
             onChangeText={nextValue =>setweightLiftingValue(nextValue)}
+            maxLength={2}
             />
 
             <Text style={{ lineHeight: 30 }}>{ }</Text>
@@ -156,6 +209,7 @@ export default NsFitPage = () => {
             label='Pushups'
             placeholder='Enter desired number of reps'
             onChangeText={nextValue =>setpushupsValue(nextValue)}
+            maxLength={2}
             />
 
             <Text style={{ lineHeight: 30 }}>{ }</Text>
@@ -166,21 +220,23 @@ export default NsFitPage = () => {
             label='Squats'
             placeholder='Enter desired number of reps'
             onChangeText={nextValue =>setsquatsValue(nextValue)}
+            maxLength={2}
             />
 
             <Text style={{ lineHeight: 30 }}>{ }</Text>
-            <Text style={{ lineHeight: 30 }}>{ }</Text>
-            <Text style={{ lineHeight: 30 }}>{ }</Text>
-            <Text style={{ lineHeight: 30 }}>{ }</Text>
 
-            <Button onPress={() => setVisible(false)}>
-              DISMISS
+            <Text style={tailwind('font-semibold text-red-600 text-center text-xs')}> Warning: Time starts 30s after pressing Begin </Text>
+            <Text style={tailwind('text-center text-sm')}> Please get into positon </Text>
+            
+            <Button 
+              onPress={() => {setVisible(false); setstartExcerciseValue(getDate())}} style={tailwind('mt-4 mx-4 rounded-lg p-4')}>
+              Begin Workout
             </Button>
+
         </Card>
         </ScrollView>
       </Modal>
       
     </Layout>
-
-    );
+   );
 };
