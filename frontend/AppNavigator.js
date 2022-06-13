72559f8b6cae6b4c8b57dfaf691b9a78bc5a6701
Mikealeee <michael.yewchuan@gmail.com>
2022-06-14 01:36:47 +0800

    Add styling
    Add styling
    Add icons
    Change colour scheme

--- frontend/AppNavigator.js
+++ frontend/AppNavigator.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
-
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -12,46 +10,42 @@ import LoginPage from "./screens/loginPage";
 import RegistrationPage from "./screens/registrationPage";
 import NsFitPage from "./screens/nsFitPage";
 
-const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const  HomeNavigator = () => {
+// BottomNavigator Bar
+const  BottomNavigator = () => {
     return(
-// put login and registration into into a startup page
-
-    <Tab.Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName="View Ippt">
+    <Tab.Navigator screenOptions={{ headerShown: false }} initialRouteName="View Ippt">
         <Tab.Screen name="Submit Ippt" component={SubmitIpptPage}  options={{ tabBarIcon: () => (
           <MaterialCommunityIcons 
           name="file-upload" 
           size={24} 
-          color="red" /> )}
-      }/>
+          color="#a12427" /> )}
+        }/>
         <Tab.Screen name="View Ippt" component={HomePage} options={{ tabBarIcon: () => (
           <MaterialCommunityIcons 
           name="home" 
           size={24} 
-          color="red" /> )}
-      }/>
+          color="#a12427" /> )}
+       }/>
         <Tab.Screen name="NsFit" component={NsFitPage} options={{ tabBarIcon: () => (
           <MaterialIcons
           name="fitness-center" 
           size={24} 
-          color="red" /> )}
-      }/>
+          color="#a12427" /> )}
+        }/>
     </Tab.Navigator>
   );
 }
 
 export const AppNavigator = () => {
   return(
-    <NavigationContainer>
+    <NavigationContainer >
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginPage} options={{headerShown: false}}/>
         <Stack.Screen name="Register" component={RegistrationPage} options={{headerShown: false}}/>
-        <Stack.Screen name="HomeNavigator" component={HomeNavigator} options={{headerShown: false}} />
+        <Stack.Screen name="HomeNavigator" component={BottomNavigator} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
     )
--- frontend/screens/nsFitPage.js
+++ frontend/screens/nsFitPage.js
@@ -3,28 +3,30 @@ import {useTailwind} from 'tailwind-rn';
 import { Layout, Text, Button, ViewPager, Modal, Card, Input} from '@ui-kitten/components';
 import { Alert, Animated, View, ScrollView } from 'react-native';
 import { Video } from 'expo-av';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
 export default NsFitPage = () => {
 
-    const video = React.useRef(null);
+    const tailwind = useTailwind();
+
     const [status, setStatus] = React.useState({});
     const [selectedIndex, setSelectedIndex] = React.useState(0);
-    const tailwind = useTailwind();
+    
+    // set modal visible or not
     const [visible, setVisible] = React.useState(false);
     
-    const [burpeesValue, setburpeesValue] = React.useState('');  // value of input from popup
-    const [weightLiftingValue, setweightLiftingValue] = React.useState('');  // value of input from popup
-    const [pushupsValue, setpushupsValue] = React.useState('');  // value of input from popup
-    const [squatsValue, setsquatsValue] = React.useState('');  // value of input from popup
-
-    const [startExcerciseValue, setstartExcerciseValue] = React.useState('');  // value of input from popup
-    const [endExcerciseValue, setendExcerciseValue] = React.useState('');  // value of input from popup
+    // varaibles used in forms
+    const [burpeesValue, setburpeesValue] = React.useState(''); 
+    const [weightLiftingValue, setweightLiftingValue] = React.useState(''); 
+    const [pushupsValue, setpushupsValue] = React.useState('');  
+    const [squatsValue, setsquatsValue] = React.useState(''); 
+    const [startExcerciseValue, setstartExcerciseValue] = React.useState('');  
+    const [endExcerciseValue, setendExcerciseValue] = React.useState('');  
     
     // get current day-month-date-time-year
     const getDate = () => {
       const currentDate = new Date;
       const localDate = currentDate.toLocaleString('en-US', 'Asia/Singapore')
-      console.log(localDate);
       return localDate;
     };
 
@@ -43,7 +45,6 @@ export default NsFitPage = () => {
         return 
       }
       setendExcerciseValue(getDate());
-
     };
 
     // submission validation
@@ -76,7 +77,6 @@ export default NsFitPage = () => {
     // begin workout form validation
     const formBeginWorkoutVerification = () => {
       if  (parseInt(burpeesValue) === 0 ) {
-        console.log(parseInt(burpeesValue))
         setburpeesValue('')
       }
       if  (  parseInt(weightLiftingValue) === 0 ) {
@@ -116,7 +116,6 @@ export default NsFitPage = () => {
  
     // reset button validation
     const resetVerification = () => {
-
       if ( true ) {
         Alert.alert(
           "Confirm reset?",
@@ -134,7 +133,6 @@ export default NsFitPage = () => {
           ],
           { cancelable: true }
       );
-
       }
     }
 
@@ -151,53 +149,81 @@ export default NsFitPage = () => {
       // pass in as props? and send to database whatever is releavant
     };
 
-    return (
-    <Layout style= {tailwind('flex-col flex-grow')} >    
-     
-      <Layout style={tailwind('bg-slate-200 items-center justify-center h-16')}> 
-        <Text style={tailwind('font-bold text-3xl')}> NS Fit </Text>  
-      </Layout>
-      
+    const Header = () => {
+        return  (     
+          <Layout style={tailwind('bg-[#a12427] items-center justify-center h-20 flex-row')}> 
+            <MaterialIcons
+            name="fitness-center" 
+            size={30} 
+            style={{transform: [{rotateY: '180deg'}]}}
+            color="white" />
+
+            <Text style={tailwind('font-bold text-3xl text-white')}>     NS Fit     </Text>  
+
+            <MaterialIcons
+            name="fitness-center" 
+            size={30} 
+            color="white" />
+
+          </Layout>
+        )
+    }
+
+    const Instructions  = () => {
+      return  ( 
       <Layout style={tailwind('items-center justify-center mt-5')}> 
         <Text style={tailwind('font-semibold text-red-800 text-xl')}> Swipe for exercise videos and follow along</Text>  
         <Text style={tailwind('font-semibold text-red-800 text-2xl mt-2')}> Tap video to play </Text>  
       </Layout>
+      )
+    }
+
+    return (
+    <Layout style= {tailwind('flex-col flex-grow')} >    
+     
+      <Header/>
+      
+      <Instructions/>
 
       <View style= {tailwind('flex-col flex-1 mt-8')}> 
-        <ViewPager selectedIndex={selectedIndex} onSelect={index => setSelectedIndex(index)} style={{ flex:1,flexGrow:1}}>
+        <ViewPager selectedIndex={selectedIndex} onSelect={index => setSelectedIndex(index)} style={{ flex:1,flexGrow:1, position: 'absolute'}}>
           <Layout style={tailwind('flex items-center justify-center mb-3')}>
+
+          <Layout style={tailwind('flex-row flex-grow items-center justify-center')}> 
               <Text style={tailwind('text-red-800 font-bold text-3xl')}> Workout Session Plan </Text>
 
+              <MaterialCommunityIcons
+              name="clipboard-outline" 
+              size={30} 
+              color="#a12427" />
+
+          </Layout>
               <Text style={{ lineHeight: 30 }}>{ }</Text>
     
-              <Card>
-                <Text> Burpees Reps  :  {burpeesValue} </Text>
-                <Text> Weights Reps  :  {weightLiftingValue} </Text>
-                <Text> Pushups Reps :  {pushupsValue} </Text>
-                <Text> Squats Reps    :  {squatsValue} </Text>
-                <Text> Start Time        :  {startExcerciseValue} </Text>
-                <Text> End Time          :  {endExcerciseValue} </Text>
+              <Card style={tailwind('bg-red-200 border-2')}>
+                <Text style={tailwind('text-[#a12427] font-bold')}> Burpees Reps  :  {burpeesValue} </Text>
+                <Text style={tailwind('text-[#a12427] font-bold')}> Weights Reps  :  {weightLiftingValue} </Text>
+                <Text style={tailwind('text-[#a12427] font-bold')}> Pushups Reps :  {pushupsValue} </Text>
+                <Text style={tailwind('text-[#a12427] font-bold')}> Squats Reps    :  {squatsValue} </Text>
+                <Text style={tailwind('text-[#a12427] font-bold')}> Start Time        :  {startExcerciseValue} </Text>
+                <Text style={tailwind('text-[#a12427] font-bold')}> End Time          :  {endExcerciseValue} </Text>
               </Card>
 
-              <Text style={{ lineHeight: 30 }}>{ }</Text>
+              <Text style={{ lineHeight: 40 }}>{ }</Text>
 
               <Layout style={tailwind('flex-row flex-1')}> 
                 <Button 
-                  onPress={() => {resetVerification()}} style={tailwind('border-red-800 bg-red-800 border-0 flex-1 mt-4 mx-8 rounded-lg p-2')}>
+                  onPress={() => {resetVerification()}} style={tailwind('border-[#a12427] bg-[#a12427] border-0 flex-1 mt-4 mx-8 rounded-lg p-2')}>
                   Reset Plan
                 </Button>
-
-                <Button onPress={() => {formSubmissionVerification()}} style={tailwind('border-red-800 bg-red-800 border-0 flex-1 mt-4 mx-8 rounded-lg p-2')}>
+                <Button onPress={() => {formSubmissionVerification()}} style={tailwind('border-[#a12427] bg-[#a12427] border-0 flex-1 mt-4 mx-8 rounded-lg p-2')}>
                   Submit Workout
                 </Button>
               </Layout>
-
           </Layout>
         
           <Layout style={tailwind('items-center justify-center')} >
-
-            <Text style={tailwind('font-bold text-3xl mb-3')}> Burpees </Text>
-
+            <Text style={tailwind('font-bold text-3xl mb-3 text-[#a12427]')}> Burpees </Text>
             <Video source={ require("../../assets/videos/burpeesVideo.mp4") }   // Can be a URL or a local file.
               rate={0.6}
               volume={1.0}
@@ -206,29 +232,26 @@ export default NsFitPage = () => {
               resizeMode="contain"
               onPlaybackStatusUpdate={(status) => setStatus(() => status)}
               isLooping
-              style={{ width: 250, height: 250,  }}
+              style={{ width: 250, height: 250, }}
             /> 
-            
           </Layout>
 
-          <Layout style={tailwind('items-center justify-center mb-3')}>
-            <Text style={tailwind('font-bold text-3xl')}> Weight Lifting </Text>
-
-              <Video source={ require("../../assets/videos/weightLiftingVideo.mp4") }   // Can be a URL or a local file.
-                rate={0.8}
-                volume={1.0}
-                isMuted={false}
-                useNativeControls
-                resizeMode="contain"
-                onPlaybackStatusUpdate={(status) => setStatus(() => status)}
-                isLooping
-                style={{ width: 250, height: 250 }}
-              /> 
-          
+          <Layout style={tailwind('items-center justify-center')}>
+            <Text style={tailwind('font-bold text-3xl mb-3 text-[#a12427]')}> Weight Lifting </Text>
+            <Video source={ require("../../assets/videos/weightLiftingVideo.mp4") }   // Can be a URL or a local file.
+              rate={0.8}
+              volume={1.0}
+              isMuted={false}
+              useNativeControls
+              resizeMode="contain"
+              onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+              isLooping
+              style={{ width: 250, height: 250 }}
+            /> 
           </Layout>
 
-          <Layout style={tailwind('items-center justify-center mb-3')}>
-            <Text style={tailwind('font-bold text-3xl')}> Pushups </Text>
+          <Layout style={tailwind('items-center justify-center')}>
+            <Text style={tailwind('font-bold text-3xl mb-3 text-[#a12427]')}> Pushups </Text>
 
             <Video source={ require("../../assets/videos/pushupsVideo.mp4") }   // Can be a URL or a local file.
               rate={1.0}
@@ -243,8 +266,8 @@ export default NsFitPage = () => {
           
           </Layout>
 
-          <Layout style={tailwind('items-center justify-center mb-3')}>
-            <Text style={tailwind('font-bold text-3xl')}> Squats </Text>
+          <Layout style={tailwind('items-center justify-center')}>
+            <Text style={tailwind('font-bold text-3xl mb-3 text-[#a12427]')}> Squats </Text>
 
             <Video source={ require("../../assets/videos/squatsVideo.mp4") }   // Can be a URL or a local file.
               rate={1.0}
@@ -262,62 +285,67 @@ export default NsFitPage = () => {
       </View>          
       
       <View style={tailwind(' flex-grow items-center justify-center flex-row mt-8 ')}>
-        <Button style={tailwind('border-red-800 bg-red-800 border-0 text-3xl mx-4 rounded-lg p-4 ')} onPress={() => setVisible(true)} > Plan Workout </Button>  
-        <Button style={tailwind('border-red-800 bg-red-800 border-0 text-3xl mx-4 rounded-lg p-4')} onPress={() => valEndExercise()} > End Workout </Button>    
+        <Button style={tailwind('border-[#a12427] bg-[#a12427] border-0 text-3xl mx-4 rounded-lg p-4 ')} onPress={() => setVisible(true)} > Plan Workout </Button>  
+        <Button style={tailwind('border-[#a12427] bg-[#a12427] border-0 text-3xl mx-4 rounded-lg p-4')} onPress={() => valEndExercise()} > End Workout </Button>    
       </View>
-
-
  
-      <Modal visible={visible} style={tailwind('h-4/5 w-4/5 mt-16 mb-0')}>
+      <Modal visible={visible} style={tailwind('mb-28 w-4/5 bg-red-100 border-2')}>
       <ScrollView >
-        <Card disabled={true}>
+        <Card disabled={true} style={tailwind('bg-red-100 border-0')}>
+            <Layout style={tailwind('bg-red-100 flex-row items-center justify-center content-center')}> 
+              <MaterialCommunityIcons
+              name="clipboard-outline" 
+              size={20} 
+             
+              color="#a12427" />
+              
+              <Text style={tailwind('font-bold text-xl text-center text-[#a12427]')}> Plan Your Exercise </Text> 
 
-            <Text style={tailwind('font-bold text-xl text-center')}> Plan Your Exercise </Text> 
-            
-            <Text style={{ lineHeight: 30 }}>{ }</Text>
+              <MaterialCommunityIcons
+              name="clipboard-outline" 
+              size={20} 
+              color="#a12427" />
+            </Layout>
 
-            <Text style={tailwind('text-center')}>  Enter the number reps you want to perform for each exercise </Text>
-
-            <Text style={{ lineHeight: 30 }}>{ }</Text>
+            <Text style={tailwind('text-center text-sm text-[#a12427]')}>  Enter the number of reps you want to perform for each exercise </Text>
             
             <Input
+            style={tailwind('bg-red-50')}
             value={burpeesValue}
             keyboardType = 'numeric'
             label='Burpees'
-            placeholder='Leave blank if not required'
+            placeholder='Leave blank if unrequired'
             onChangeText={nextValue =>setburpeesValue(nextValue.replace(/[^0-9]/g, ''))}
             maxLength={2}
+            
             />
             
-            <Text style={{ lineHeight: 30 }}>{ }</Text>
-
             <Input
+            style={tailwind('bg-red-50')}
             value={weightLiftingValue}
             keyboardType = 'numeric'
             label='Weight Lifting'
-            placeholder='Leave blank if not required'
+            placeholder='Leave blank if unrequired'
             onChangeText={nextValue =>setweightLiftingValue(nextValue.replace(/[^0-9]/g, ''))}
             maxLength={2}
             />
 
-            <Text style={{ lineHeight: 30 }}>{ }</Text>
-
             <Input
+            style={tailwind('bg-red-50')}
             value={pushupsValue}
             keyboardType = 'numeric'
             label='Pushups'
-            placeholder='Leave blank if not required'
+            placeholder='Leave blank if unrequired'
             onChangeText={nextValue =>setpushupsValue(nextValue.replace(/[^0-9]/g, ''))}
             maxLength={2}
             />
 
-            <Text style={{ lineHeight: 30 }}>{ }</Text>
-
             <Input
+            style={tailwind('bg-red-50')}
             value={squatsValue}
             keyboardType = 'numeric'
             label='Squats'
-            placeholder='Leave blank if not required'
+            placeholder='Leave blank if unrequired'
             onChangeText={nextValue =>setsquatsValue(nextValue.replace(/[^0-9]/g, ''))}
             maxLength={2}
             />
@@ -325,13 +353,17 @@ export default NsFitPage = () => {
             <Text style={{ lineHeight: 30 }}>{ }</Text>
 
             <Text style={tailwind('font-semibold text-red-600 text-center text-xs')}> Warning: Time starts 30s after pressing Begin </Text>
+            <Text style={{ lineHeight: 30 }}>{ }</Text>
             <Text style={tailwind('text-center text-sm')}> Please get into positon </Text>
             
-            <Layout style={tailwind('flex-1 flex-row')}>
-              <Button onPress={() => {formBeginWorkoutVerification()}} style={tailwind('border-red-800 bg-red-800 items-center justify-center flex-1 mt-4 mx-4 rounded-lg p-2 border-0')}>
+            <Text style={{ lineHeight: 30 }}>{ }</Text>
+     
+
+            <Layout style={tailwind('flex-1 flex-row bg-red-100 border-0')}>
+              <Button onPress={() => {formBeginWorkoutVerification()}} style={tailwind('border-[#a12427] bg-[#a12427] items-center justify-center flex-1 mt-4 mx-4 rounded-lg p-2 border-0')}>
                 <Text style={tailwind('items-center justify-center')}>   Begin Workout </Text>
               </Button>
-              <Button onPress={() => {setVisible(false); resetExcercisePlan()}} style={tailwind('border-red-800 bg-red-800 flex-1 mt-4 mx-4 rounded-lg p-2 border-0')}>
+              <Button onPress={() => {setVisible(false); resetExcercisePlan()}} style={tailwind('border-[#a12427] bg-[#a12427] flex-1 mt-4 mx-4 rounded-lg p-2 border-0')}>
                 Dismiss
               </Button>
             </Layout>
