086bc9c1fc6794181bd4a1f41119acc457eea289
Jonathan <joncwr99@gmail.com>
2022-06-14 23:43:45 +0800

    Add validation and change icon for login page

--- frontend/screens/validationSchema.js
+++ frontend/screens/validationSchema.js
@@ -4,21 +4,30 @@ const isTwoDigit = (value) => /^[1-9]?\d$/.test(value);
 const isSeconds = (value) => /^[0-5]?[0-9]$/.test(value);
 
 const ipptSchema = Yup.object().shape({
-      runningMin: Yup.string()
-      .test("Minutes", isTwoDigit)
-      .required("Required")
-      .test("Minutes", "Enter a valid duration", val => parseInt(val) < 25 ),
-      runningSec: Yup.string()
-      .test("Seconds", isSeconds)
-      .required("Required"),
-      pushup: Yup.string()
-      .test("Push Up", "Must be a positive integer", isTwoDigit)
-      .test("Sit Up", "Number cannot be more than 60", val => parseInt(val) < 61 )
-      .required("Required"),
-      situp: Yup.string()
-      .test("Sit Up", "Must be a positive integer", isTwoDigit)
-      .test("Sit Up", "Number cannot be more than 60", val => parseInt(val) < 61 )
-      .required("Required"),
+        runningMin: Yup.string()
+        .test("Minutes", isTwoDigit)
+        .required("Required")
+        .test("Minutes", "Enter a valid duration", val => parseInt(val) < 25 ),
+        runningSec: Yup.string()
+        .test("Seconds", isSeconds)
+        .required("Required"),
+        pushup: Yup.string()
+        .test("Push Up", "Must be a positive integer", isTwoDigit)
+        .test("Sit Up", "Number cannot be more than 60", val => parseInt(val) < 61 )
+        .required("Required"),
+        situp: Yup.string()
+        .test("Sit Up", "Must be a positive integer", isTwoDigit)
+        .test("Sit Up", "Number cannot be more than 60", val => parseInt(val) < 61 )
+        .required("Required"),
   });
 
-  export{ipptSchema}
+
+const loginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email!").required("Required"),
+    password: Yup.string()
+        .min(6, "minimum 6 characters")
+        .max(50, "Maximum 50 characters")
+        .required("Required"),
+});
+
+export{ipptSchema, loginSchema}
